12d459c2af7c34d3d46a53ab5ccba5b4a77fc4d0
Micha Vie <micha@vleap.io>
2024-06-02 16:59:00 +0200

    update test setup

--- tests/register_tests.rs
+++ tests/register_tests.rs
@@ -1,10 +1,18 @@
+use multiversx_sc::types::*;
+use multiversx_sc_scenario::api::*;
 use setup::*;
 
 pub mod setup;
 
 #[test]
-fn it_registers() {
+fn it_registers_an_app() {
     let mut contract = TestContract::new();
 
-    assert!(true);
+    let receipt_token_name = ManagedBuffer::<StaticApi>::from(b"Receipt");
+    let receipt_token_ticker = ManagedBuffer::<StaticApi>::from(b"RECEIPT");
+    let contribution_token = TokenIdentifier::<StaticApi>::from("CONTR-123456");
+
+    contract.register_app(contribution_token, receipt_token_name, receipt_token_ticker);
+
+    assert!(true); // figure out how to check storage in type safe way
 }
--- tests/setup.rs
+++ tests/setup.rs
@@ -1,12 +1,14 @@
 use multiversx_sc_scenario::imports::*;
+use strategy_contribution_token::*;
 
 pub const OWNER_ADDRESS: TestAddress = TestAddress::new("owner");
+pub const APP_ADDRESS: TestAddress = TestAddress::new("app");
 pub const STRATEGY_ADDRESS: TestSCAddress = TestSCAddress::new("strategy");
 pub const CODE_PATH: MxscPath = MxscPath::new("output/adder.mxsc.json");
 
 pub fn blockchain() -> ScenarioWorld {
     let mut blockchain = ScenarioWorld::new();
-    blockchain.register_contract(CODE_PATH, strategy_token::ContractBuilder);
+    blockchain.register_contract(CODE_PATH, strategy_contribution_token::ContractBuilder);
 
     blockchain
 }
@@ -18,7 +20,35 @@ pub struct TestContract {
 impl TestContract {
     pub fn new() -> Self {
         let chain = blockchain();
+        let mut contract = Self { chain };
 
-        Self { chain }
+        contract.chain.account(APP_ADDRESS).nonce(1).balance(1000);
+        contract.chain.account(OWNER_ADDRESS).nonce(1).balance(1000);
+
+        contract.deploy();
+
+        contract
+    }
+
+    pub fn register_app(&mut self, contribution_token: TokenIdentifier<StaticApi>, receipt_token_name: ManagedBuffer<StaticApi>, receipt_token_ticker: ManagedBuffer<StaticApi>) {
+        self.chain
+            .tx()
+            .from(APP_ADDRESS)
+            .to(STRATEGY_ADDRESS)
+            .typed(strategy_contribution_token_proxy::StrategyContractProxy)
+            .register_app_endpoint(contribution_token, receipt_token_name, receipt_token_ticker)
+            .egld(1)
+            .run();
+    }
+
+    fn deploy(&mut self) {
+        self.chain
+            .tx()
+            .from(OWNER_ADDRESS)
+            .typed(strategy_contribution_token_proxy::StrategyContractProxy)
+            .init()
+            .code(CODE_PATH)
+            .new_address(STRATEGY_ADDRESS)
+            .run();
     }
 }
